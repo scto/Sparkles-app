cca1609fc32187269b29868517fa6a2703d5a9d6
NEOAPPS <158327205+neoapps-dev@users.noreply.github.com>
2025-03-25 23:02:09 +0300

    fix: compile error due to missing import

--- app/src/main/java/com/sparkleside/ui/activities/HtmlViewerActivity.java
+++ app/src/main/java/com/sparkleside/ui/activities/HtmlViewerActivity.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import android.graphics.Bitmap;
 import java.io.OutputStream;
+import android.content.Context;
 
 public class HtmlViewerActivity extends AppCompatActivity {
     private ActivityHtmlViewerBinding binding;
