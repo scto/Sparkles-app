ff24df1f3b0d2f49d079a2037ed7a681c8badafa
NEOAPPS <158327205+neoapps-dev@users.noreply.github.com>
2025-03-25 20:23:56 +0300

    Added a WebServer instead of data:text/html, to make Eruda work

--- app/src/main/java/com/sparkleside/ui/activities/HtmlViewerActivity.java
+++ app/src/main/java/com/sparkleside/ui/activities/HtmlViewerActivity.java
@@ -7,14 +7,22 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import java.io.IOException;
 import java.io.InputStream;
-import androidx.appcompat.app.AppCompatActivity;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
 import com.google.android.material.transition.platform.MaterialContainerTransform;
 import com.google.android.material.transition.platform.MaterialContainerTransformSharedElementCallback;
 import com.sparkleside.R;
 import com.sparkleside.databinding.ActivityHtmlViewerBinding;
+import androidx.appcompat.app.AppCompatActivity;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.Executors;
 
 public class HtmlViewerActivity extends AppCompatActivity {
   private ActivityHtmlViewerBinding binding;
+  private HttpServer server;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -33,33 +41,63 @@ public class HtmlViewerActivity extends AppCompatActivity {
     getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     getSupportActionBar().setHomeButtonEnabled(true);
     binding.toolbar.setNavigationOnClickListener(v -> onBackPressed());
+    startServer();
     setupWebView();
-    binding.webview.loadUrl("data:text/html,".concat(getIntent().getStringExtra("html")));
+    binding.webview.loadUrl("http://127.0.0.1:11001");
+  }
+
+  private void startServer() {
+    try {
+      server = HttpServer.create(new InetSocketAddress(11001), 0);
+      server.createContext("/", new HttpHandler() {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+          String html = getIntent().getStringExtra("html");
+          byte[] response = html.getBytes(StandardCharsets.UTF_8);
+          exchange.sendResponseHeaders(200, response.length);
+          OutputStream os = exchange.getResponseBody();
+          os.write(response);
+          os.close();
+        }
+      });
+      server.setExecutor(Executors.newSingleThreadExecutor());
+      server.start();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
   private void injectEruda() {
-        try {
-            InputStream is = getAssets().open("eruda.min.js");
-            int size = is.available();
-            byte[] buffer = new byte[size];
-            is.read(buffer);
-            is.close();
-            String jsCode = new String(buffer);
-            binding.webview.loadUrl("javascript:(function() {" + jsCode + " eruda.init(); })();");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    try {
+      InputStream is = getAssets().open("eruda.min.js");
+      int size = is.available();
+      byte[] buffer = new byte[size];
+      is.read(buffer);
+      is.close();
+      String jsCode = new String(buffer);
+      binding.webview.loadUrl("javascript:(function() {" + jsCode + " eruda.init(); })();");
+    } catch (IOException e) {
+      e.printStackTrace();
     }
-  
+  }
+
   private void setupWebView() {
-        WebSettings webSettings = binding.webview.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        binding.webview.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                super.onPageFinished(view, url);
-                injectEruda();
-            }
-        });
+    WebSettings webSettings = binding.webview.getSettings();
+    webSettings.setJavaScriptEnabled(true);
+    binding.webview.setWebViewClient(new WebViewClient() {
+      @Override
+      public void onPageFinished(WebView view, String url) {
+        super.onPageFinished(view, url);
+        injectEruda();
+      }
+    });
+  }
+
+  @Override
+  protected void onDestroy() {
+    if (server != null) {
+      server.stop(0);
     }
+    super.onDestroy();
+  }
 }
