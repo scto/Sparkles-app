1fc6256dfaf796048c19dd93e0e259152027b0fb
NEOAPPS <158327205+neoapps-dev@users.noreply.github.com>
2025-03-25 23:27:21 +0300

    try(fix): Eruda

--- app/src/main/java/com/sparkleside/ui/activities/HtmlViewerActivity.java
+++ app/src/main/java/com/sparkleside/ui/activities/HtmlViewerActivity.java
@@ -3,6 +3,7 @@ package com.sparkleside.ui.activities;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.*;
 import android.webkit.WebViewClient;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.transition.platform.MaterialContainerTransform;
